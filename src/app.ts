6e926405f111b4b9bd12d79c630bcd3d32993710
Mxt@DEV <maxta.gamer@gmail.com>
2024-08-14 19:53:24 -0300

    update

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata';
+import 'express-async-errors';
+import express, { json } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+export const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(json());
